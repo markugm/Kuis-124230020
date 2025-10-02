185b0cd8bcb435bb74a02789a1935d08dcebd9cf
markugm <sania.dinara22@gmail.com>
2025-10-02 08:34:27 +0700

    .

---
+++ kuis_124230020/lib/login_page.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'main.dart'; 
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>(); 
+  //pake textcontroller biar lebih efektif
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  
+  // ngatur visibilitas password
+  bool _isPasswordVisible = false; 
+
+  void _attemptLogin() {
+    if (_formKey.currentState!.validate()) {
+      if (_usernameController.text == 'dinara' && _passwordController.text == '123') {
+        // snackbar login berhasil floating
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Selamat datang ${_usernameController.text}!"),
+            backgroundColor: Colors.green,
+            behavior: SnackBarBehavior.floating,
+          ),
+        );
+
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => HomePage(username: _usernameController.text),
+          ),
+        );
+      } else {
+        // snackbar login gagal floating juga
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Username atau Password salah!"),
+            backgroundColor: Colors.redAccent,
+            behavior: SnackBarBehavior.floating,
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(32.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.directions_car_filled, 
+                  size: 100, 
+                  color: primaryColor, 
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Vehicle ShowApp',
+                  style: TextStyle(
+                    fontSize: 24, 
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                
+                // Input Username
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: accentColor, width: 2.0), // Aksen saat fokus
+                    ),
+                    prefixIcon: const Icon(Icons.person, color: primaryColor),
+                  ),
+                  validator: (value) => value!.isEmpty ? 'Masukkan username Anda' : null,
+                ),
+                const SizedBox(height: 20),
+                
+                // Input Password
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: !_isPasswordVisible, // opsi Hide/View
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: accentColor, width: 2.0),
+                    ),
+                    prefixIcon: const Icon(Icons.lock, color: primaryColor),
+                    // Tombol Toggle Password
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        color: Colors.grey,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  validator: (value) => value!.isEmpty ? 'Masukkan password Anda' : null,
+                ),
+                const SizedBox(height: 40),
+                
+                // button Login
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: _attemptLogin,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primaryColor,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: const Text(
+                      'Login', 
+                      style: TextStyle(
+                        fontSize: 18, 
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        letterSpacing: 1.5, 
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
